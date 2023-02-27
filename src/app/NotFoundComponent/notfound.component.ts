791c1805df726e9aa92980022d29e0bb83cc0093
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2023-02-27 14:53:20 +0300

    initial commit

---
+++ src/app/NotFoundComponent/notfound.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.css']
+})
+export class NotfoundComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {PortfolioComponent} from "./PortfolioComponent/portfolio.component";
+import {NotfoundComponent} from "./NotFoundComponent/notfound.component";
+
+const routes: Routes = [
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
